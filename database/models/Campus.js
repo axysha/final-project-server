478928c2c48620c7ec370f41ddb62bca30ebcaad
IvyS <ivysg0817@gmail.com>
2025-05-15 16:05:01 -0400

    Resolve merge conflict in Campus.js

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -3,8 +3,8 @@
 
 It defines the campus model for the database.
 ==================================================*/
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require('sequelize');       // Import Sequelize
+const db        = require('../db');           // Import Sequelize database instance called "db"
 
 // Define the campus model
 const Campus = db.define("campus", {
@@ -30,3 +30,4 @@ const Campus = db.define("campus", {
 });
 
 module.exports = Campus;
+
